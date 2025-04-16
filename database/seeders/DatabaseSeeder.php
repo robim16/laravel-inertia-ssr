4801f899b58cc4fa57eee18761d333684ce5c218
robim16 <luis.hoyos@dydsoluciones.net>
2025-04-16 15:43:03 -0500

    creacion de feature item comp.

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enum\PermissionsEnum;
+use App\Enum\RolesEnum;
+use App\Models\Feature;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+       
+        $userRole = Role::create(['name' => RolesEnum::User->value]);
+        $commenterRole = Role::create(['name' => RolesEnum::Commenter->value]);
+        $adminRole = Role::create(['name' => RolesEnum::Admin->value]);
+
+        $manageFeaturesPermission = Permission::create([
+            'name' => PermissionsEnum::ManageFeatures->value
+        ]);
+
+        $manageCommentsPermission = Permission::create([
+            'name' => PermissionsEnum::ManageComments->value
+        ]);
+
+        $manageUsersPermission = Permission::create([
+            'name' => PermissionsEnum::ManageUsers->value
+        ]);
+
+        $upvoteDownvotePermission = Permission::create([
+            'name' => PermissionsEnum::UpvoteDownvote->value
+        ]);
+
+        $userRole->syncPermissions([$upvoteDownvotePermission]);
+        $commenterRole->syncPermissions([$upvoteDownvotePermission, $manageCommentsPermission]);
+        $adminRole->syncPermissions([
+            $manageFeaturesPermission,
+            $manageCommentsPermission,
+            $manageUsersPermission,
+            $upvoteDownvotePermission
+        ]);
+
+
+        User::factory()->create([
+            'name' => 'User User',
+            'email' => 'user@example.com',
+        ])->assignRole(RolesEnum::User);
+
+        User::factory()->create([
+            'name' => 'Commenter User',
+            'email' => 'commenter@example.com',
+        ])->assignRole(RolesEnum::Commenter);
+
+        User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+        ])->assignRole(RolesEnum::Admin);
+
+        Feature::factory(100)->create();
+    }
+}
---
+++ resources/js/Components/FeatureItem.tsx
@@ -0,0 +1,11 @@
+import { Feature } from "@/types";
+
+export default function FeatureItem({ feature }: { feature: Feature }) {
+    return (
+        <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
+            <div className="p-6 text-gray-900 dark:text-gray-100">
+                
+            </div>
+        </div>
+    )
+}
