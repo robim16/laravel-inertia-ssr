ec7e3b63fda483c5e21b7860fcab3d8bedf06fbb
robim16 <carteagajimenez@gmail.com>
2025-05-18 15:38:24 -0500

    corrección en rutas web

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-
-    Route::middleware(['verified', 'role'.RolesEnum::Admin->value])->group(function () {
+    Route::middleware(['verified', 'role:' . RolesEnum::Admin->value])->group(function () {
         Route::get('/user', [UserController::class, 'index'])
             ->name('user.index');
-
         Route::get('/user/{user}/edit', [UserController::class, 'edit'])
             ->name('user.edit');
-
         Route::put('/user/{user}', [UserController::class, 'update'])
             ->name('user.update');
     });
@@ -42,9 +39,9 @@ Route::middleware('auth')->group(function () {
             return Inertia::render('Dashboard');
         })->name('dashboard');
 
-        Route::resource('/feature', FeatureController::class)
+        Route::resource('feature', FeatureController::class)
             ->except(['index', 'show'])
-            ->middleware('can'.PermissionsEnum::ManageFeatures->value);
+            ->middleware('can:' . PermissionsEnum::ManageFeatures->value);
 
         Route::get('/feature', [FeatureController::class, 'index'])
             ->name('feature.index');
@@ -52,7 +49,7 @@ Route::middleware('auth')->group(function () {
         Route::get('/feature/{feature}', [FeatureController::class, 'show'])
             ->name('feature.show');
 
-        Route::post('/feature/{feature}/upvote', [UpvoteController::class], 'store')
+        Route::post('/feature/{feature}/upvote', [UpvoteController::class, 'store'])
             ->name('upvote.store');
 
         Route::delete('/upvote/{feature}', [UpvoteController::class, 'destroy'])
@@ -60,11 +57,11 @@ Route::middleware('auth')->group(function () {
 
         Route::post('feature/{feature}/comments', [CommentController::class, 'store'])
             ->name('comment.store')
-            ->middleware('can'.PermissionsEnum::ManageComments->value);
+            ->middleware('can:' . PermissionsEnum::ManageComments->value);
 
         Route::delete('comment/{comment}', [CommentController::class, 'destroy'])
             ->name('comment.destroy')
-            ->middleware('can'.PermissionsEnum::ManageComments->value);
+            ->middleware('can:' . PermissionsEnum::ManageComments->value);
     });
 });
 
